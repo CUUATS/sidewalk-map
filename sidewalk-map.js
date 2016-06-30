4eb68cc319757cab068d1f13272807d7df916bca
Matt Yoder <myoder@ccrpc.org>
2016-06-30 12:20:51 -0500

    Initial work on adding individual feature layers.

--- sidewalk-map.js
+++ sidewalk-map.js
@@ -1,22 +1,26 @@
 require([
-  // 'esri/tasks/ClassBreaksDefinition',
   'esri/renderers/ClassBreaksRenderer',
+  'dojo/_base/Color',
   'esri/layers/FeatureLayer',
   'esri/layers/LayerDrawingOptions',
   'esri/dijit/Legend',
   'esri/map',
+  'esri/symbols/SimpleLineSymbol',
+  'esri/symbols/SimpleMarkerSymbol',
   'dojo/_base/array',
   'dojo/parser',
   'esri/request',
   'dojo/domReady!'
 ],
 function(
-  // ClassBreaksDefinition,
   ClassBreaksRenderer,
+  Color,
   FeatureLayer,
   LayerDrawingOptions,
   Legend,
   Map,
+  SimpleLineSymbol,
+  SimpleMarkerSymbol,
   array,
   parser,
   request) {
@@ -24,6 +28,67 @@ function(
   var AGG_URL = 'http://utility.arcgis.com/usrsvcs/servers/06fb34d68e7a4673ba885015b671387b/rest/services/CCRPC/SidewalkInventoryScoreAggregated/MapServer',
     AGG_DEFAULT_FEATURE = 'Sidewalk',
     AGG_DEFAULT_FIELD = 'ScoreCompliance',
+    IND_URL = 'http://utility.arcgis.com/usrsvcs/servers/88a6a9f6dc45461f820659d2d0f13fff/rest/services/CCRPC/SidewalkInventoryScore/MapServer',
+    BREAKS = [
+      {
+        maxValue: 60,
+        label: '0 to 60',
+        color: '#e61c1f'
+      },
+      {
+        maxValue: 70,
+        label: '> 60 to 70',
+        color: '#ffb061'
+      },
+      {
+        maxValue: 80,
+        label: '> 70 to 80',
+        color: '#fcfc5d'
+      },
+      {
+        maxValue: 90,
+        label: '> 80 to 90',
+        color: '#80cee8'
+      },
+      {
+        maxValue: 100,
+        label: '> 90 to 100',
+        color: '#2e80bf'
+      }
+    ],
+    makeIndLayer = function(idx, markerType, markerSize) {
+      var layer = new FeatureLayer(IND_URL + '/' + idx, {
+        mode: FeatureLayer.MODE_ONDEMAND,
+        outFields: ['*']
+      });
+      layer.setScaleRange(10001, 0);
+      layer.setRenderer(makeIndRenderer(markerType, markerSize));
+      return layer;
+    },
+    makeIndRenderer = function(markerType, markerSize) {
+      var renderer = new ClassBreaksRenderer(
+        makeMarkerSymbol(markerType, markerSize, '#bbbbbb'),
+        'ScoreCompliance');
+      array.forEach(BREAKS, function(b, i) {
+        renderer.addBreak(
+          (i > 0) ? BREAKS[i-1].maxValue + 0.000001 : 0,
+          b.maxValue,
+          makeMarkerSymbol(markerType, markerSize, b.color));
+      });
+      return renderer;
+    },
+    makeMarkerSymbol = function(markerType, markerSize, fillColor) {
+      return new SimpleMarkerSymbol(
+        markerType,
+        markerSize,
+        new SimpleLineSymbol(
+          SimpleLineSymbol.STYLE_SOLID,
+          new Color('#000000'),
+          1
+        ),
+        new Color(fillColor)
+      );
+    }
     map = new Map('map', {
       center: [-88.2, 40.1],
       zoom: 11,
@@ -34,6 +99,10 @@ function(
       outFields: ['*'],
       opacity: 0.5
     }),
+    crLayer = makeIndLayer(0, SimpleMarkerSymbol.STYLE_CIRCLE, 10),
+    // cwLayer = makeIndLayer(1, SimpleMarkerSymbol.STYLE_CROSS, 10),
+    // psLayer = makeIndLayer(2, SimpleMarkerSymbol.STYLE_DIAMOND, 10),
+    // swLayer = makeIndLayer(3, SimpleMarkerSymbol.STYLE_CIRCLE, 10),
     fieldName = document.getElementById('fieldName'),
     featureType = document.getElementById('featureType'),
     updateButton = document.getElementById('updateMap'),
@@ -63,7 +132,7 @@ function(
       legend.startup();
     });
     map.addLayer(aggLayer);
-    // map.addLayer(crLayer);
+    map.addLayer(crLayer);
 
     request({
       url: AGG_URL + '/0',
